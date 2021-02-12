bf0594ada95c627a90dbad14d045ddc9a5243089
Edgaras zarskij <edgaraszarskij1@gmail.com>
2021-02-12 11:34:56 +0200

    Initial commit

--- server/handlers/error.js
+++
@@ -1,7 +0,0 @@
- const errorHandler = (error, request, response, next) => {
-    return response.status(error.status || 500).json({
-        message: error.message || " Something went wrong"
-    })
-}
-
-module.exports = errorHandler;
--- server/routes/viewed.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {viewed} =  require('../handlers/viewedHandler')
-
-router.post('/viewed',viewed)
-
-module.exports = router;
